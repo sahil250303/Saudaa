943d591f02c4d383c235f273f03886d910051111
Sahil Reghate <sahilreghate@gmail.com>
2026-05-30 23:55:37 +0530

    Increased Express json and urlencoded body parser payload limit to 10mb to support large Base64 image uploads

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const activeAdminTokens = new Set();
 
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const { readDB, writeDB } = require('./db.js');
